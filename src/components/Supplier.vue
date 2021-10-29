21c742786f8f5b90ddd3c29ffba9b400b02b2bec
Clowiser <jessica.rigaud@le-campus-numerique.fr>
2021-10-29 13:34:12 +0200

    Passage de props au composant Supplier

--- src/components/Supplier.vue
+++ src/components/Supplier.vue
@@ -5,7 +5,7 @@
             <!-- quand !status = false grâce au ! et sans ! = true -->
             <h3>A du stock? {{status}}</h3>
         </div>
-        <h5>Date de dernier relevé des stocks: {{checkedAt.toLocaleString()}}</h5>
+        <!-- <h5>Date de dernier relevé des stocks: {{checkedAt.toLocaleString()}}</h5> --> 
     </div>
 </template>
 
@@ -14,14 +14,15 @@
 // 5.1.2 - Ce composant aura comme data l'objet javascript suivant :
 export default {
     name: 'Supplier',
-
-    data(){
-        return {
-            name: 'Mon fournisseur',
-            status: true, // est ce qu'il y a du stock
-            checkedAt: new Date() // date de la dernière mise à jour du stock
-        }
-    }
+    props: ['name', 'status', 'checkedAt'],
+    //   data(){
+    //     return {
+    //         name: 'Mon fournisseur',
+    //         status: true, // est ce qu'il y a du stock
+    //         checkedAt: new Date() // date de la dernière mise à jour du stock
+    //     }
+    // }
+    // 5.2.2 - Ajoutez le fait que le composant Supplier a trois props qui s'appellent name, status et checkedAt, supprimez les datas du composant Supplier, mais ajoutez les dans le composant SuppliersList.
 }
 
 
