da6d20d4351bd8e7b8928b0e77861488f5264b87
Clowiser <jessica.rigaud@le-campus-numerique.fr>
2021-10-29 10:24:27 +0200

    Ajout sur GitHub

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <img
+    alt="Vue logo"
+    src="./assets/spiruline.png"
+  />
+  <HelloWorld msg="La Spiruline" />
+  <div> 
+    <!-- BOUTONS -->
+    <input type="button" v-on:click="onSuppliersListClick" value="Liste des fournisseurs"/>
+    <input type="button" @click="onMapClick" value="Carte des fournisseurs"/>
+  </div>
+</template>
+
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  components: {
+    HelloWorld,
+  },
+
+   methods: { // on déclare nos méthodes dans un methods
+    onSuppliersListClick(){
+      alert('Message sur la liste');
+    },
+
+    onMapClick(){
+      alert('Message sur la carte');
+    },
+}
+};
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+</template>
+
+<script>
+export default {
+  props: ["msg"],
+  data() {
+    return {
+      count: 1,
+    };
+  },
+};
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
